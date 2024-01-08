7a7b7c1601f5f93a69ab103f532f74409e374a59
Holger Stanke <holger.stanke@db.com>
2024-01-08 16:58:41 +0100

    BANKAPI-14453 Change year from 2023 to 2024

--- tutorials/simple-java-app/src/main/java/com/db/bankapi/codesample/CallDbApiCashAccount.java
+++ tutorials/simple-java-app/src/main/java/com/db/bankapi/codesample/CallDbApiCashAccount.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright 2023 Deutsche Bank AG
+ *  Copyright 2024 Deutsche Bank AG
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
